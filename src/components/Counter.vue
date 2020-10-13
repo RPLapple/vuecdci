704703c6d9780ef0e1eb256a08f744df04686fe3
GQSM <ms314006@hotmail.com>
2020-10-13 20:32:33 +0800

    add: Counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>當前點擊次數{{ count }}</h1>
+    <button @click="increment">點我加1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count += 1;
+    },
+  },
+};
+</script>
