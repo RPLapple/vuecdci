b70dbc9c9872a78eb9854bfb7c1307d10a5272ee
GQSM <ms314006@hotmail.com>
2020-10-13 21:36:33 +0800

    add 減一的按鈕

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -2,6 +2,7 @@
   <div class="hello">
     <h1 data-testId="currentCounter">當前點擊次數{{ count }}</h1>
     <button @click="increment">點我加1</button>
+    <button @click="increment">點我減1</button>
   </div>
 </template>
 
